544efddb8905035dfc52b1a3924649fbca8ada0b
hobbit1983 <hobbit1983@gmail.com>
2016-11-06 23:06:47 +0000

    Add maximiser function

--- minimiser.playground/Contents.swift
+++ minimiser.playground/Contents.swift
@@ -27,8 +27,33 @@ func minimiser(inputstr: String) ->String{
         }
     }
 
-    return createNewString(numbers: characters, posA: bestPosA, posB: bestPosB)
+    return "Min " + createNewString(numbers: characters, posA: bestPosA, posB: bestPosB)
 }
+
+func maximiser(inputstr: String) ->String{
+    print("Resolving for String: " + inputstr)
+    let characters = Array(inputstr.characters)
+    var currentMinimum = Int.min
+    var bestPosA = 0;
+    var bestPosB = 0;
+    
+    for index1 in 1...characters.count-1{
+        for index2 in index1...characters.count-1{
+            if index1 == index2{
+                continue
+            }
+            let currentAttempt = calculate(numbers: characters, posA: index1, posB: index2)
+            if currentAttempt > currentMinimum{
+                currentMinimum = currentAttempt
+                bestPosA = index1
+                bestPosB = index2
+            }
+        }
+    }
+    
+    return "Max: " + createNewString(numbers: characters, posA: bestPosA, posB: bestPosB)
+}
+
 
 func calculate(numbers: Array<Character>, posA: Int, posB: Int) ->Int {
     //carve the string into 3 numbers at point A and point B
@@ -51,5 +76,7 @@ func createNewString(numbers: Array<Character>,posA: Int, posB: Int) -> String{
 }
 
 print(minimiser(inputstr: "123456789"))
+print(maximiser(inputstr: "123456789"))
 print(minimiser(inputstr: "8924658371"))
+print(maximiser(inputstr: "8924658371"))
 
